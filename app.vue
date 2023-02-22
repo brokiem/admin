e877e3336d8e781fbc47d953e186ef79053ba472
brokiem <milyhqvideo@gmail.com>
2023-02-22 16:08:31 +0800

    Use lanyard wrapper websocket

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
         <div class="flex flex-row mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8 gap-4">
           <h1 class="text-3xl font-bold tracking-tight text-gray-900">Admin Panel</h1>
 
-          <button @click="refresh" type="button" class="items-center rounded-md border border-indigo-600 px-2 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-indigo-500 focus:ring-0 focus:ring-offset-2 transition duration-300">
+          <button v-if="showRefreshButton" @click="refresh" type="button" class="items-center rounded-md border border-indigo-600 px-2 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-700 hover:text-white focus:outline-none focus:ring-indigo-500 focus:ring-0 focus:ring-offset-2 transition duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
               <path fill-rule="evenodd" d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z" clip-rule="evenodd" />
             </svg>
@@ -77,12 +77,7 @@ import {
 } from "~/composables/states";
 import AddAnimeModal from "~/components/AddAnimeModal.vue";
 import LatestProjectSelector from "~/components/LatestProjectSelector.vue";
-
-const { pending, data: lanyard, refresh } = useFetch(() => 'https://api.lanyard.rest/v1/users/548120702373593090')
-
-if (!pending.value) {
-  refresh()
-}
+import { connectWebSocket, fetchUserData } from "lanyard-wrapper";
 
 const selectedMenu = useSelectedMenu()
 const projects = useProjects()
@@ -91,17 +86,32 @@ const anime = useAnime()
 const modalOpen = useModalOpen()
 const setSecretKey = useSetSecret()
 
+let showRefreshButton = false;
+
+const refresh = () => {
+  if (process.client) {
+    fetchUserData("548120702373593090").then((data) => {
+      projects.value = JSON.parse(data.kv.projects);
+      latestProject.value = JSON.parse(data.kv.latest_project);
+      anime.value = JSON.parse(data.kv.fav_anime);
+    });
+  }
+}
+
 const openModal = () => {
   modalOpen.value = true
 }
 
-watch(lanyard, (newLanyard) => {
-  projects.value = JSON.parse(newLanyard.data.kv.projects)
-
-  latestProject.value = JSON.parse(newLanyard.data.kv.latest_project)
-
-  anime.value = JSON.parse(newLanyard.data.kv.fav_anime)
-})
+if (process.client) {
+  connectWebSocket("548120702373593090", (data) => {
+    projects.value = JSON.parse(data.kv.projects);
+    latestProject.value = JSON.parse(data.kv.latest_project);
+    anime.value = JSON.parse(data.kv.fav_anime);
+  }, () => {
+    refresh();
+    showRefreshButton = true;
+  });
+}
 
 function selectMenu(menu) {
   selectedMenu.value = menu
